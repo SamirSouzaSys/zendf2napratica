3d756c148aa6f25ee70fad0cd32d507da000388c
samir.guitar <samir.guitar@gmail.com>
2014-03-11 11:03:25 -0300

    20140311

--- module/Application/data/test.data.php
+++ module/Application/data/test.data.php
@@ -1,40 +1,33 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 //queries used by tests
 return array(
     'posts' => array(
-        'create' => 'CREATE TABLE if not existis posts(
-            id INT NOT NULL AUTO_INCREMENT,
-            title VARCHAR(250) NOT NULL,
-            description TEXT NOT NULL,
-            post_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                PRIMARY KEY(id) )
-            ENGINE = InnoDB;',
-        'drop' => "DROP TABLE posts;"
+        'create' => 'CREATE  TABLE if not exists posts (
+                      id INT NOT NULL AUTO_INCREMENT ,
+                      title VARCHAR(250) NOT NULL ,
+                      description TEXT NOT NULL ,
+                      post_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+                      PRIMARY KEY (id) )
+                    ENGINE = InnoDB;',
+        'drop' => 'drop table posts;'
     ),
     'comments' => array(
-        'create' => 'CREATE TABLE if not exists comments (
-            id INT NOT NULL AUTO_INCREMENT ,
-            post_id INT NOT NULL ,
-            description TEXT NOT NULL ,
-            name VARCHAR(200) NOT NULL ,
-            email VARCHAR(250) NOT NULL ,
-            webpage VARCHAR(200) NOT NULL ,
-            comment_date TIMESTAMP NULL ,
-                PRIMARY KEY (id, post_id) ,
-                INDEX fk_comments_posts (post_id ASC) ,
-                CONSTRAINT fk_comments_posts
-                FOREIGN KEY (post_id )
-                REFERENCES posts (id )
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION)
-            ENGINE = InnoDB;',
+        'create' => 'CREATE  TABLE if not exists comments (
+                      id INT NOT NULL AUTO_INCREMENT ,
+                      post_id INT NOT NULL ,
+                      description TEXT NOT NULL ,
+                      name VARCHAR(200) NOT NULL ,
+                       email VARCHAR(250) NOT NULL ,
+                     webpage VARCHAR(200) NOT NULL ,
+                      comment_date TIMESTAMP NULL ,
+                        PRIMARY KEY (id, post_id) ,
+                        INDEX fk_comments_posts (post_id ASC) ,
+                      CONSTRAINT fk_comments_posts
+                        FOREIGN KEY (post_id )
+                            REFERENCES posts (id )
+                            ON DELETE NO ACTION
+                           ON UPDATE NO ACTION)
+                    ENGINE = InnoDB;',
         'drop' =>'drop table comments;'
-    ),
+    )
 );
--- module/Application/tests/src/Application/Model/PostTest.php
+++ module/Application/tests/src/Application/Model/PostTest.php
@@ -1,116 +1,126 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Application\Model;
-
+ 
 use Core\Test\ModelTestCase;
 use Application\Model\Post;
 use Zend\InputFilter\InputFilterInterface;
-
+ 
 /**
  * @group Model
  */
-
-class PostTest extends ModelTestCase{
-    public function testGetInputFilter(){
+class PostTest extends ModelTestCase
+{
+    public function testGetInputFilter()
+    {
         $post = new Post();
         $if = $post->getInputFilter();
         //testa se existem filtros
-        $this->assertInstanceOf("Zend\InputFilter",$if);
+        $this->assertInstanceOf("Zend\InputFilter\InputFilter", $if);
         return $if;
     }
+ 
     /**
      * @depends testGetInputFilter
      */
-    public function testInputFilterValid($if){
+    public function testInputFilterValid($if)
+    {
         //testa os filtros
-        $this->assertEquals(4,$if->count());
-        
+        $this->assertEquals(4, $if->count());
+ 
         $this->assertTrue($if->has('id'));
         $this->assertTrue($if->has('title'));
         $this->assertTrue($if->has('description'));
         $this->assertTrue($if->has('post_date'));
     }
+    
     /**
-     * @expctedException Core\Model\EntityException
+     * @expectedException Core\Model\EntityException
      */
-    public function testInputFilterinvalido(){
+    public function testInputFilterInvalido()
+    {
         //testa se os filtros estão funcionando
         $post = new Post();
         //title só pode ter 100 caracteres
-        $post->title = 'Lorem Ipsum e simplesmente uma simulacao de texto da industria tipografica e de impressos. Lorem Ipsum é simplesmente uma simulacao de texto da industria tipografica e de impressos';
-    }
+        $post->title = 'Lorem Ipsum é simplesmente uma simulação de texto da indústria 
+        tipográfica e de impressos. Lorem Ipsum é simplesmente uma simulação de texto 
+        da indústria tipográfica e de impressos';
+    }        
+ 
     /**
-    * Teste de insercao de um post valido
-    */
-    public function testInsert(){
+     * Teste de inserção de um post válido
+     */
+    public function testInsert()
+    {
         $post = $this->addPost();
+ 
         $saved = $this->getTable('Application\Model\Post')->save($post);
-
+ 
         //testa o filtro de tags e espaços
-        $this->assertEquals('A Apple compra a Coderock', $saved->description);
-        //testa o auto increment da chave primaria
-        $this->assertEquals(1,$saved->id);
+        $this->assertEquals('A Apple compra a Coderockr', $saved->description);
+        //testa o auto increment da chave primária
+        $this->assertEquals(1, $saved->id);
     }
-
+ 
     /**
-    * @expectedException Core\Model\EntityException
-    * @expectedExceptionMessage Input inválido: description = 
-    */
-
-    public function testInsertInvalido(){
+     * @expectedException Core\Model\EntityException
+     * @expectedExceptionMessage Input inválido: description = 
+     */
+    public function testInsertInvalido()
+    {
         $post = new Post();
         $post->title = 'teste';
-        $post->description ='';
-
+        $post->description = '';
+ 
         $saved = $this->getTable('Application\Model\Post')->save($post);
-    }
-
-    public function testUpdate(){
+    }    
+ 
+    public function testUpdate()
+    {
         $tableGateway = $this->getTable('Application\Model\Post');
         $post = $this->addPost();
-
+ 
         $saved = $tableGateway->save($post);
         $id = $saved->id;
-
-        $this->assertEquals(1,$id);
-
+ 
+        $this->assertEquals(1, $id);
+ 
         $post = $tableGateway->get($id);
-        $this->assertEquals('Apple compra a Coderock',$post->title);
-
-        $post->title = 'Coderock compra a Apple';
+        $this->assertEquals('Apple compra a Coderockr', $post->title);
+ 
+        $post->title = 'Coderockr compra a Apple';
         $updated = $tableGateway->save($post);
-        
+ 
         $post = $tableGateway->get($id);
-        $this->assertEquals('Coderock compra a Apple',$post->title);
+        $this->assertEquals('Coderockr compra a Apple', $post->title);
     }
-
+ 
     /**
-    * @expectedException Core\Model\EntityException
-    * @expectedExceptionMessage Could not find row 1
-    */
-    public function testDelete(){
+     * @expectedException Core\Model\EntityException
+     * @expectedExceptionMessage Could not find row 1
+     */
+    public function testDelete()
+    {
         $tableGateway = $this->getTable('Application\Model\Post');
         $post = $this->addPost();
-
+ 
         $saved = $tableGateway->save($post);
         $id = $saved->id;
-
+ 
         $deleted = $tableGateway->delete($id);
-        $this->assertEquals(1,$deleted); //número de linhas excluídas
+        $this->assertEquals(1, $deleted); //numero de linhas excluidas
+ 
+        $post = $tableGateway->get($id);
     }
-
-    private function addPost(){
+ 
+    private function addPost()
+    {
         $post = new Post();
         $post->title = 'Apple compra a Coderockr';
         $post->description = 'A Apple compra a <b>Coderockr</b><br> ';
         $post->post_date = date('Y-m-d H:i:s');
-
+ 
         return $post;
     }
+ 
 }
+?>
